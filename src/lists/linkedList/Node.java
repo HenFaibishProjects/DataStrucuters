338b632830182e42789fb0499ca4667a5ff33bf5
hen.faibish@solaredge.com <hen.faibish@solaredge.com>
2019-03-07 16:23:39 +0200

    String

---
+++ src/lists/linkedList/Node.java
@@ -0,0 +1,5 @@
+package lists.linkedList;
+
+public class Node {
+
+}
---
+++ src/mainApp.java
@@ -0,0 +1,33 @@
+import lists.linkedList.LinkedList;
+import string.StringInterviewQuestions;
+
+public class mainApp {
+    public static char[] myvar= new char[]{'K', 'E', 'V', 'I', 'N'};
+
+    public static void main(String args[]) {
+        //           simplearray.overAllArray();
+//          selectionSort.sortBySelection(simplearray.intArray);
+//          simplearray.printArray(simplearray.intArray);
+//          bubbleSort.sortByBubble(simplearray.intArray);
+//          simplearray.printArray(simplearray.intArray);
+//        	System.out.println(Recorsion.interivelFindHowManyZeroOnArray(simplearray.intArray));
+//          ArrayInterviewQuestions.findMissingNumber(simplearray.intOngoingArray);
+//          ArrayInterviewQuestions.findLargestAndSmallest(simplearray.intArray);
+//            ArrayInterviewQuestions.findIfArrayContainsNumber(simplearray.stringArray);
+//
+//            Lists.createEployeeListWithData();
+//            Lists.printEployeeList();
+        StringInterviewQuestions stringInterviewQuestions = new StringInterviewQuestions();
+        stringInterviewQuestions.proveStringsImmutable();
+        //LinkedList.createEployeeLinkedList();
+//            LinkedList.addEployeeLinkedList();
+//            LinkedList.removeFirstItem();
+//            LinkedList.printSizeOfLinkedList();
+//              LinkedList.printLinkedList();
+        //         JdkLinedList list = new JdkLinedList();
+        //list.printTheList();
+
+
+    }
+
+}
